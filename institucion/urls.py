12cefd218ad23f002d7517712c7b0238eff88ba2
luisma <mrluisgamerps3@gmail.com>
2026-04-10 17:34:54 -0400

    XDDDDD

--- institucion/urls.py
+++ institucion/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', include('institucion.core.urls')),
 ]
 
 if settings.DEBUG:
