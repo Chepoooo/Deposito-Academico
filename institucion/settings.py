70a44ae903367a06c9a3879b06226143443c4570
luisma <mrluisgamerps3@gmail.com>
2026-04-10 18:39:59 -0400

    back to sqlite

--- institucion/settings.py
+++ institucion/settings.py
@@ -82,9 +82,8 @@ WSGI_APPLICATION = 'institucion.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+        'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+
 }
 
 
