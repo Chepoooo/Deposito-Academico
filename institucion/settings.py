e9aa4785272f1ffe4ddee2b9de2b699617e31da3
luisma <mrluisgamerps3@gmail.com>
2026-04-10 17:23:40 -0400

    institucion issue

--- institucion/settings.py
+++ institucion/settings.py
@@ -40,10 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'academico',
-    'materiales',
-    'core',
+    'institucion.academico',
+    'institucion.materiales',
+    'institucion.core',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
